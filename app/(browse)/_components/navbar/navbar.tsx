b842b4d24915ea2e4b91f3bd3341f8b59f951d49
csametsahin <mcnblogger@gmail.com>
2024-03-09 13:42:25 +0300

    add sidebar remove package lock

--- app/(browse)/_components/navbar/navbar.tsx
+++ app/(browse)/_components/navbar/navbar.tsx
@@ -1,15 +1,56 @@
+import { PanelBottomOpen } from "lucide-react";
 import UserAvatar from "./user-avatar";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
+import PagesSection from "../sidebar/pages-section";
+import SocialsSection from "../sidebar/socials-section";
 
 const Navbar = () => {
   return (
-    <nav className=" mt-20 mx-40 bg-gradient-to-b overflow-hidden justify-between rounded-md border p-5 hidden lg:flex ">
-      <div className="flex flex-col space-y-4">
+    <nav className=" lg:mt-10 lg:mx-40 bg-gradient-to-b overflow-hidden justify-between rounded-md  border p-5  lg:flex ">
+      <div className=" flex-col space-y-4 hidden lg:flex">
         <div>
           <UserAvatar />
         </div>
       </div>
-      <div>
+      <div className="hidden lg:flex">
         <h2 className="text-xl">csametdevcom</h2>
+      </div>
+      <div className="flex lg:hidden items-center justify-center">
+        <Drawer>
+          <DrawerTrigger asChild>
+            <Button variant="ghost">
+              <PanelBottomOpen />
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent>
+            <div className="mx-auto w-full max-w-sm">
+              <DrawerHeader>
+                <UserAvatar />
+              </DrawerHeader>
+              <div className="p-4 pb-0">
+                <div className="flex items-center justify-center space-x-2">
+                  <div className="flex-1 text-center">
+                    <PagesSection />
+                  </div>
+                </div>
+                <div className="mt-3 h-[120px]"></div>
+              </div>
+              <DrawerFooter>
+                <SocialsSection />
+              </DrawerFooter>
+            </div>
+          </DrawerContent>
+        </Drawer>
       </div>
     </nav>
   );
--- app/(browse)/page.tsx
+++ app/(browse)/page.tsx
@@ -1,13 +1,24 @@
+import Image from "next/image";
+import csametsahin from "@/public/csametsahin.jpg";
+
 export default function Home() {
   return (
     <main className="flex mx-2 lg:ml-20 lg:mr-40 flex-col overflow-hidden py-2 space-y-2">
       <div className="flex overflow-hidden space-x-2">
+        <Image
+          className="rounded-full hidden lg:flex"
+          width={150}
+          height={150}
+          src={csametsahin}
+          alt="CSŞ"
+        />
+
         <p>
-          Hi 👋 I'm <strong>Samet</strong>. Currently working as Software
+          Hi 👋 I&apos;m <strong>Samet</strong>. Currently working as Software
           Engineer at{" "}
           <a href="https://www.arcelikglobal.com/en/" className="font-semibold">
             Arçelik Global
-          </a>
+          </a>{" "}
           I am working as Full Stack Developer, using .NET and React
         </p>
       </div>
