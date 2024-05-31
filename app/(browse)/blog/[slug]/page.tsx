f7a41a1e70f9ae5c108786fd9f63becc6c243b04
26100000 <26100000@arcelik.com>
2024-05-31 16:45:47 +0300

    impl contentful v1

--- app/(browse)/blog/[slug]/page.tsx
+++ app/(browse)/blog/[slug]/page.tsx
@@ -2,12 +2,12 @@ import { getPostById } from "@/actions/postActions";
 
 interface BlogPageProps {
   params: {
-    id: string;
+    slug: string;
   };
 }
 
 const Page = async ({ params }: BlogPageProps) => {
-  const post = await getPostById(params.id);
+  const post = await getPostById(params.slug);
   return <div className="m-2">{post}</div>;
 };
 
--- components/post.tsx
+++ components/post.tsx
@@ -10,21 +10,29 @@ import {
   CardTitle,
 } from "./ui/card";
 
-import nextsvg from "@/public/next.svg";
+import { redirect } from "next/navigation";
 
-type CardProps = React.ComponentProps<typeof Card>;
+export type PostProps = {
+  title: string;
+  slug: string;
+  author: string;
+  date: string;
+  coverImage: string;
+};
+
+type CardProps = React.ComponentProps<typeof Card> & PostProps;
 
 const Post = ({ className, ...props }: CardProps) => {
   return (
     <Card className={cn("w-200", className)} {...props}>
       <CardHeader>
-        <CardTitle>Post Title</CardTitle>
+        <CardTitle>{props.title}</CardTitle>
         <CardDescription>post description.</CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
         <div className=" flex items-center lg:space-x-4 rounded-md border p-4 overflow-hidden">
           <div className="space-y-1 hidden lg:flex w-40 h-40">
-            <Image src={nextsvg} alt="image" />
+            <Image src={props.coverImage} alt="image" />
           </div>
           <div className="flex-1 space-y-1">
             <p className="text-sm lg:font-medium leading-none font-light">
@@ -36,7 +44,12 @@ const Post = ({ className, ...props }: CardProps) => {
         </div>
       </CardContent>
       <CardFooter>
-        <Button className="ml-auto">Read</Button>
+        <Button
+          className="ml-auto"
+          onClick={() => redirect(`/posts/${props.slug}`)}
+        >
+          Read
+        </Button>
       </CardFooter>
     </Card>
   );
